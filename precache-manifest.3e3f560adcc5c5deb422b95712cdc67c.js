00e403350ccc20ecc54480440089376e6550f45d
jjohnsundar <jjohnsundar9943@gmail.com>
2020-06-01 14:23:14 +0530

    Updates

---
+++ precache-manifest.3e3f560adcc5c5deb422b95712cdc67c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427ecbd0809ad29d15001bfc78491204",
+    "url": "/Scientificcalculator/index.html"
+  },
+  {
+    "revision": "d03101b5cb998d6437f2",
+    "url": "/Scientificcalculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "625d9421f551e2ccb35c",
+    "url": "/Scientificcalculator/static/js/2.4906145d.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Scientificcalculator/static/js/2.4906145d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d03101b5cb998d6437f2",
+    "url": "/Scientificcalculator/static/js/main.ab04c6b7.chunk.js"
+  },
+  {
+    "revision": "ee61790276e6e84593d2",
+    "url": "/Scientificcalculator/static/js/runtime-main.17abcb7a.js"
+  }
+]);
